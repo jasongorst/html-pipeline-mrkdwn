d86d20533e57563aa2f3920102393e4cc07295c5
Jason Gorst <jason.gorst@me.com>
2023-04-05 01:55:52 -0500

    use minitest default reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,4 +4,5 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "html/pipeline/mrkdwn"
 
 require "minitest/autorun"
-require "minitest/rg"
+require "minitest/reporters"
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new]
